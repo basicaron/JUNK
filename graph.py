034204c782e077e4cb599fb75126624752a0880b
unknown <ronldmhrb@gmail.com>
2020-08-16 19:43:26 +0530

    Sleep

--- graph.py
+++ graph.py
@@ -1,1 +1,8 @@
 import numpy as np
+from PyQt5.QtWidgets import QWidget,QApplication,QVBoxLayout,QLabel
+class window(QWidget):
+    def __init__(self):
+        super().__init__()
+    
+    def initUI(self):
+        pass
