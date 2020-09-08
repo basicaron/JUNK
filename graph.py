eda891f505016d4f360069eb608cdcf0c92f61f1
unknown <ronldmhrb@gmail.com>
2020-09-08 21:25:24 +0530

    night

--- graph.py
+++ graph.py
@@ -8,6 +8,7 @@ class window(QWidget):
         self.setGeometry(100,15,600,500)
         self.Abc = []
         self.newnode = False
+        self.move = False
         self.screensize = [res.width(),res.height()]
         self.Abc.append(['first',[int(self.screensize[0]/2),int(self.screensize[1]/8)],[100,50],None])
         self.InitUI()
@@ -31,21 +32,32 @@ class window(QWidget):
         if self.newnode:
             temp = self.node_form(self.mousepos)
             paint.drawRect(temp[1][0]-temp[2][0],temp[1][1]-temp[2][1],temp[2][0]*2,temp[2][1]*2)
+        if self.move:
+            self.selectednode[1] = [self.mousepos.x(),self.mousepos.y()]
 
     def mousePressEvent(self,event):
         if self.newnode:
             self.newnode = False
             self.Abc.append(self.node_form(event.pos()))
             self.update()
-        print(self.Abc)
+        if self.move:
+            self.move = False
+            self.update()
 
     def contextMenuEvent(self,event):
-        menu = QMenu(self)
-        new = QAction("New")
-        new.triggered.connect(self.new_node)
-        menu.addAction(new)
-        menu.exec(self.mapToGlobal(event.pos()))
-
+        self.selectednode = self._mousePoint(event.pos())
+        if self.selectednode == None:
+            menu = QMenu(self)
+            new = QAction("New")
+            new.triggered.connect(self._new_node)
+            menu.addAction(new)
+            menu.exec(self.mapToGlobal(event.pos()))
+        else:
+            menu = QMenu(self)
+            move = QAction("Move")
+            move.triggered.connect(self._move_node)
+            menu.addAction(move)
+            menu.exec(self.mapToGlobal(event.pos()))
     def mouseMoveEvent(self,event):
         self.mousepos = event.pos()
         self.update()
@@ -58,8 +70,17 @@ class window(QWidget):
         temp.append([100,50])
         temp.append(None)
         return temp
-
-    def new_node(self):
+    def _mousePoint(self,pos):
+        A = None
+        for temp in self.Abc:
+            if temp[1][0]-temp[2][0]<pos.x() and temp[1][0]+temp[2][0]>pos.x():
+                if temp[1][1]-temp[2][1]<pos.y() and temp[1][1]+temp[2][1]>pos.y():
+                    A = temp
+        return A
+    def _move_node(self):
+        self.setMouseTracking(True)
+        self.move = True
+    def _new_node(self):
         self.setMouseTracking(True)
         self.newnode = True
 app = QApplication([])
